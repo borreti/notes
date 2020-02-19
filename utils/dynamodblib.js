b8824ad1a12e257c4a9c18697b507e488d6a118f
Robert Victor <robert.cunha@enacom.com.br>
2020-02-19 18:29:56 -0300

    .

---
+++ utils/dynamodblib.js
@@ -0,0 +1,15 @@
+import AWS from 'aws-sdk';
+
+
+/**
+ * Runs a dynamoDb operation
+ * @export
+ * @param {*} action
+ * @param {*} params
+ * @return {*}
+ */
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
